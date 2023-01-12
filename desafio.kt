5fb5c3c5238b59ee10e4379f7ce5dab52bccff5d
Natália da Silva Paes Govoni <106287307+naspaes@users.noreply.github.com>
2023-01-11 21:23:10 -0300

    Update desafio.kt
    Desafio atualizado.

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,86 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { Básico, Intermediário, Avançado }
 
-class Usuario
+data class Usuario (val nome: String, val email: String) 
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
-    val inscritos = mutableListOf<Usuario>()
+    val alunos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+         alunos.add(usuario)
+    }
+    
+    fun imprimirConteudo (){
+     for (tudo in conteudos){
+         println ("${tudo.nome} - ${tudo.duracao}")
+         } 
+    }
+    
+    fun imprimirAlunos (){
+     for ( all in alunos){
+        println(all.nome)
+       }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+   
+    
+    //Conteúdos Educacionais
+    val Android1 = ConteudoEducacional ( "Introdução ao Desenvolvimento Mobile Nativo para Android", 40,)
+    val Android2 = ConteudoEducacional ( "Desenvolvimento Android - Ambiente de Desenvolvimento", 60) 
+    val Android3 = ConteudoEducacional ( "Estrutura de Projetos Android", 80)
+    val Web1 = ConteudoEducacional ("Aprendendo HTML", 40)
+    val Web2 = ConteudoEducacional ("Aprendendo CSS",  60)
+    val Web3 = ConteudoEducacional ("Aprendendo HTML",  80)
+    val Java1 = ConteudoEducacional ("Fundamentos da Plataforma Java", 40)
+    val Java2 = ConteudoEducacional ("Programação Orientada a Objetos em Java", 60)
+    val Java3 = ConteudoEducacional ("Utilizando Spring Framework", 80)
+    
+    // Criando Listas de Conteúdos Educacionais
+    val listAndroid = mutableListOf<ConteudoEducacional>()
+        listAndroid.add(Android1)
+        listAndroid.add(Android2)
+        listAndroid.add(Android3)
+        
+    val listWeb = mutableListOf<ConteudoEducacional>()
+        listWeb.add(Web1)
+        listWeb.add(Web2)
+        listWeb.add(Web3)
+    
+     val listJava = mutableListOf<ConteudoEducacional>()
+        listJava.add(Java1)
+        listJava.add(Java2)
+        listJava.add(Java3)
+     
+    
+    // Formações
+    val androidDeveloper = Formacao ("Formação Android Developer", listAndroid, Nivel.Intermediário) 
+    val webDeveloper = Formacao ("Formação Web Developer", listWeb, Nivel.Básico) 
+    val javaDeveloper = Formacao ("Formação Java Developer", listJava, Nivel.Avançado) 
+    
+     
+    // Usuários
+    val Natalia = Usuario ("Natália", "naspaes@gmail.com")
+    val Gustavo = Usuario ("Gustavo", "gustavo@gmail.com")
+    val Viviane = Usuario ("Viviane", "vivi@gmail.com")
+    
+    // Matriculando os alunos
+    androidDeveloper.matricular(Natalia)
+    webDeveloper.matricular(Gustavo)
+    javaDeveloper.matricular(Viviane)
+    
+    // Imprimindo as saídas
+    
+    print("Formação DIO: ")
+    println("${androidDeveloper.nome} - ${androidDeveloper.nivel}")
+    println("Conteúdo Educacional: ")
+    println(androidDeveloper.imprimirConteudo())
+    println()
+    println("Alunos matriculados: ")
+    println(androidDeveloper.imprimirAlunos())
 }
